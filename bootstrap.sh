cbe3c05cfd6659bbcd4a03d9e1d6b41d914e66ca
Jason Peak <jpeak5@lsu.edu>
2016-03-02 16:07:39 -0600

    remove old provisioner file

--- bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-sudo apt-get update
-sudo apt-get install -y libxml2-dev libxslt-dev python-dev python-pip python-lxml zlib1g-dev pdftohtml
-sudo pip install --upgrade lxml scraperwiki
